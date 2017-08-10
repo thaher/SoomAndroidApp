953e70ce78672d9cdfaab06c57676f8cecddbfad
thaher <thaher.m@bridge-india.in>
2017-08-10 18:34:03 +0530

    bug fixin --testhg - change pass

--- app/src/main/java/com/bridge/soom/Fragment/AccountFragment.java
+++ app/src/main/java/com/bridge/soom/Fragment/AccountFragment.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.telephony.PhoneNumberUtils;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,12 +23,14 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.bridge.soom.Activity.RegistrationFillActivity;
 import com.bridge.soom.Helper.NetworkManager;
 import com.bridge.soom.Helper.SharedPreferencesManager;
+import com.bridge.soom.Interface.ChangePassResponse;
 import com.bridge.soom.Interface.ProviderDetailsResponse;
 import com.bridge.soom.Model.UserModel;
 import com.bridge.soom.R;
@@ -45,7 +49,7 @@ import static com.bridge.soom.Helper.Constants.DEVICE_ID;
  * Use the {@link AccountFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AccountFragment extends Fragment implements ProviderDetailsResponse {
+public class AccountFragment extends Fragment implements ProviderDetailsResponse,ChangePassResponse {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -65,7 +69,8 @@ public class AccountFragment extends Fragment implements ProviderDetailsResponse
     private Snackbar snackbar;
     private ViewGroup hiddenPanel;
     private ImageButton close_popup;
-
+    private EditText confirmpass,newpass,oldpass;
+    private Button fab;
     private boolean isEditing =false;
 
 
@@ -128,6 +133,17 @@ public class AccountFragment extends Fragment implements ProviderDetailsResponse
         hiddenPanel = (ViewGroup)view.findViewById(R.id.hidden_panel);
         hiddenPanel.setVisibility(View.INVISIBLE);
         close_popup = (ImageButton) view.findViewById(R.id.close_popup);
+        fab = (Button) view.findViewById(R.id.fab);
+        confirmpass = (EditText) view.findViewById(R.id.confirmpass);
+        newpass = (EditText) view.findViewById(R.id.newpass);
+        oldpass = (EditText) view.findViewById(R.id.oldpass);
+
+        confirmpass.setTypeface(Typeface.DEFAULT);
+        confirmpass.setTransformationMethod(new PasswordTransformationMethod());
+        newpass.setTypeface(Typeface.DEFAULT);
+        newpass.setTransformationMethod(new PasswordTransformationMethod());
+        oldpass.setTypeface(Typeface.DEFAULT);
+        oldpass.setTransformationMethod(new PasswordTransformationMethod());
 
         code.setText("+91");
 
@@ -177,6 +193,40 @@ public class AccountFragment extends Fragment implements ProviderDetailsResponse
 
                 }
                 slideUpDown(v);
+
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try  {
+                    InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+                } catch (Exception e) {
+
+                }
+
+                if(allvalid()){     String old = oldpass.getText().toString();
+                    String newp = newpass.getText().toString();
+                    if (old.trim().isEmpty() || newp.trim().isEmpty() || confirmpass.toString().trim().isEmpty()) {
+                        Snackbar.make(view, "Invalid Fields!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    } else {
+                        oldpass.setText("");
+                        newpass.setText("");
+                        confirmpass.setText("");
+                        showLoadingDialog();
+                        networkManager.new SetNewPasswordTask(AccountFragment.this, tocken, old, newp, "")
+                                .execute();
+
+                    }
+                }
+
+
+
+
+
 
             }
         });
@@ -256,6 +306,67 @@ public class AccountFragment extends Fragment implements ProviderDetailsResponse
         }
     }
 
+    private boolean allvalid() {
+        if(oldpass.getText().toString().trim().isEmpty())
+        {
+            oldpass.setError("Invalid Field");
+
+            //snackbar
+            snackbar = Snackbar.make(view, "Invalid Field", Snackbar.LENGTH_LONG);
+            View snackBarView = snackbar.getView();
+            snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
+            snackbar.show();
+
+            return false;
+        }
+        else if (newpass.getText().toString().trim().isEmpty())
+        {
+            newpass.setError("Invalid Field");
+
+
+            //snackbar
+            snackbar = Snackbar.make(view, "Invalid Field", Snackbar.LENGTH_LONG);
+            View snackBarView = snackbar.getView();
+            snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
+            snackbar.show();
+            return false;
+        }
+        else if (newpass.getText().toString().trim().length()<6)
+        {
+            newpass.setError("Requires Minimus 6 character");
+
+
+            //snackbar
+            snackbar = Snackbar.make(view, "Requires Minimus 6 character", Snackbar.LENGTH_LONG);
+            View snackBarView = snackbar.getView();
+            snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
+            snackbar.show();
+            return false;
+        }
+        else if( confirmpass.getText().toString().trim().isEmpty())
+        {
+            //snackbar
+            confirmpass.setError("Invalid Field");
+
+            snackbar = Snackbar.make(view, "Invalid Field", Snackbar.LENGTH_LONG);
+            View snackBarView = snackbar.getView();
+            snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
+            snackbar.show();
+            return false;
+        }
+        else if(!newpass.getText().toString().equals(confirmpass.getText().toString()))
+        {
+            confirmpass.setError("Passwords does not match!");
+
+            //snackbar
+            snackbar = Snackbar.make(view, "Passwords does not match!", Snackbar.LENGTH_LONG);
+            View snackBarView = snackbar.getView();
+            snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
+            snackbar.show();
+
+            return false;
+        }
+        return true;    }
 
     private boolean validateForm() {
 
@@ -386,6 +497,36 @@ public class AccountFragment extends Fragment implements ProviderDetailsResponse
         View snackBarView = snackbar.getView();
         snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
         snackbar.show();
+    }
+
+    @Override
+    public void changePassResponseSuccess(String message) {
+        //snackbar
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                dismissLoadingDialog();
+
+                snackbar = Snackbar.make(view, "Password Changed Successfully!", Snackbar.LENGTH_LONG);
+                View snackBarView = snackbar.getView();
+                snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
+                snackbar.show();
+            }});
+
+
+    }
+
+    @Override
+    public void changePassResponseFailed(String message) {
+        //snackbar
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() { dismissLoadingDialog();
+
+                snackbar = Snackbar.make(view, "Passwords Change Failed!", Snackbar.LENGTH_LONG);
+                View snackBarView = snackbar.getView();
+                snackBarView.setBackgroundResource(R.color.colorPrimaryDark);
+                snackbar.show();}});
     }
 
     @Override
